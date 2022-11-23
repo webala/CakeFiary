56b7aaaf75265d0e71e8abe15eb469089f30e6a2
webala <webd01@protonmail.com>
2022-11-23 12:45:06 +0300

    style: NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import style from './NavBar.module.scss'
-import {BsInfoCircle, BsTelephone, BsCart, BsSearch}  from 'react-icons/bs'
+import {BsInfoCircle, BsTelephone, BsCart, BsSearch, BsFacebook, BsInstagram}  from 'react-icons/bs'
 import {CgMenuBoxed} from 'react-icons/cg'
+import Link from 'next/link'
+import { FiTwitter } from 'react-icons/fi'
 
 
 function NavBar() {
   return (
-    <div className={style.navbar}>
-        <div className={style.nav_item}>
-            <p>Menu</p>
-            <CgMenuBoxed className='md:hover:animate-bounce'/>
+    <div className={style.navbar_container}>
+        <div className={style.navbar}>
+            <div className={`${style.nav_item} group`}>
+                <p>Menu</p>
+                <CgMenuBoxed className='md:group-hover:animate-bounce'/>
+            </div>
+            <div className={`${style.nav_item} group`}>
+                <p>Contact</p>
+                <BsTelephone className='md:group-hover:animate-bounce'/>
+            </div>
+            <div className={style.heading}>
+                <h1>Cake Fairy</h1>
+            </div>
+            <div className={`${style.nav_item} group`}>
+                <p>Cart</p>
+                <BsCart className='md:group-hover:animate-bounce'/>
+            </div>
+            <div className={`${style.nav_item} group`}>
+                <p>Search</p>
+                <BsSearch className='md:group-hover:animate-bounce'/>
+            </div>
         </div>
-        <div className={style.nav_item}>
-            <p>Contact</p>
-            <BsTelephone className='md:hover:animate-bounce'/>
-        </div>
-        <div className={style.heading}>
-            <h1>Cake Fairy</h1>
-        </div>
-        <div className={style.nav_item}>
-            <p>Cart</p>
-            <BsCart className='md:hover:animate-bounce'/>
-        </div>
-        <div className={style.nav_item}>
-            <p>Search</p>
-            <BsSearch className='hover:animate-bounce'/>
+        <div className={style.social_links}>
+            <Link href="#" className='hover:animate-bounce'><BsFacebook /></Link>
+            <Link href="#" className='hover:animate-bounce'><BsInstagram /></Link>
+            <Link href="#" className='hover:animate-bounce'><FiTwitter /></Link>
         </div>
     </div>
   )
