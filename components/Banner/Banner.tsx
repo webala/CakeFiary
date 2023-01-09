09343aaf6ab7c01e13e18e4066c9ffcf8dd726bc
webala <webd01@protonmail.com>
2023-01-09 12:23:21 +0300

    refactor: menu

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./Banner.module.scss";
 import Image from "next/image";
-import bg from "../../public/breads.jpg";
+import bg from "../../public/banner.jpg";
 import { GrFormNextLink } from "react-icons/gr";
 
 function Banner() {
@@ -9,12 +9,14 @@ function Banner() {
 		<div className={style.banner}>
 			<h1>Memorable Taste Made With Love</h1>
 			<p>
-				The perfect cake everytime speciaally for you from our oven to yout door.
+				The perfect cake everytime speciaally for you from our oven to yout
+				door.
 			</p>
-      <button>
-        <p>Order Now</p>
-        <GrFormNextLink className={style.icon} />
-      </button>
+			<button>
+				<p>Order Now</p>
+				<GrFormNextLink className={style.icon} />
+			</button>
+			<Image src={bg} width={768} alt="banner" className={style.image} />
 		</div>
 	);
 }
--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import Category from './components/Category'
-import style from "./Menu.module.scss"
+import React from "react";
+import Category from "./components/Category";
+import style from "./Menu.module.scss";
+import Image from "next/image";
+import { BsCartPlus } from "react-icons/bs";
 
+function Menu({ categories, flavours }: { categories: []; flavours: [] }) {
+	console.log("flavours: ", flavours);
+	return (
+		<div className={style.menu_container}>
+			<h1 className={style.title}>What We Make</h1>
 
+			<div className={style.flavours}>
+				{flavours.map((flavour) => {
+					return (
+						<div className={style.flavour}>
+							<Image
+								src={flavour.image}
+								alt={`${flavour.name}`}
+								width={700}
+								height={700}
+								className={style.image}
+							/>
 
+							<div className={style.name}>
+								<h1>{flavour.name}</h1>
+								{/* <BsCartPlus className={style.icon}/> */}
+							</div>
 
-function Menu({categories, flavours}: {categories: [], flavours: []}) {
-  return (
-    <div className={style.menu_container}>
-      <h1>Menu</h1>
-      <div className={style.menu}>
-        {categories.map((category:Object, index:number) => {
-          return <Category key={index} flavours={flavours} category={category}/>
-        })}
-      </div>
-    </div>
-  )
+							<p>
+								Starting @ ksh <span> {flavour.category.point_five}</span>
+							</p>
+
+              <button>Order now</button>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default Menu
+export default Menu;
