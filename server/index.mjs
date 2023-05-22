a45d00a455df52e5f973e86eb927bac81e3ea61d
webala <webdspam@gmail.com>
2023-05-22 10:10:29 +0300

    initialized the backend server

---
+++ server/index.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+   res.send("Hello world")
+})
+
+const port = 8000
+
+app.listen(port, () => {
+   console.log(`Server ready at ${port}`)
+})
